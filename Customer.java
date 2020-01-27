2baa0d18476dde3097aa8d66b691e7b468214dea
Krishna Kumar <krishna@Krishnas-MacBook-Pro.local>
2020-01-26 20:33:35 -0500

    New File

---
+++ Customer.java
@@ -0,0 +1,17 @@
+package HWTasks;
+
+public class Customer {
+	
+	public	String acctNum;
+	public String name;
+	public	double balance;
+	public	String acctType;
+	
+	void printCustomerDetails()
+	{
+		System.out.println(acctNum+" "+name+ " "+balance+" "+acctType);
+	}
+	
+	
+
+}
